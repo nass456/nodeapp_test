4b0ac35f6066bb497ffa50c2faf789f31363c524
AndRog <rognetta.andrea@yahoo.com>
2023-04-06 12:30:59 +0200

    change  response
    change response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Corso DevOps" }');
+    res.send('{ "response": "Ci ammazziamo di pipeline!" }');
 });
 
 app.get('/will', function (req, res) {
