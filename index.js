9e5c3362761060a9c9ffb1f7c55b9e35fec6a188
AndRog <rognetta.andrea@yahoo.com>
2023-04-06 12:24:23 +0200

    change string
    modify string get request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From AndreaRognetta1" }');
+    res.send('{ "response": "Hello From Corso DevOps" }');
 });
 
 app.get('/will', function (req, res) {
