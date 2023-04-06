5dfb45c00e59f97d8d3eb2a9edf9942f6ea6d334
nass456 <118133369+nass456@users.noreply.github.com>
2023-04-06 17:48:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Ci ammazziamo di pipeline!" }');
+    res.send('{ "response": "Nazzareno!" }');
 });
 
 app.get('/will', function (req, res) {
